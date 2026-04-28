9647722d7ba3f0922345bb1eec05f9027f37d9e7
XBauw <X.Bauwelinckx@gmail.com>
2026-04-28 14:40:29 +0200

    Update Blinker.h
    Updated Blinker class to include start delay parameter in constructor.

--- Blinker.h
+++ Blinker.h
@@ -5,12 +5,12 @@
     class Blinker {
     private:
         unsigned long _volgendeBlinkOm;
-        unsigned long _nu;
+        unsigned long _startDelay;
         unsigned long _blinkInterval;
         unsigned int _ledPin; 
     public:
         //constructor
-        Blinker(unsigned int ledPin, unsigned long blinkInterval);
+        Blinker(unsigned int ledPin, unsigned long blinkInterval, const long _startDelay = 0);
         //destructor
         ~Blinker();
 
